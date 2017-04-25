4581f88cd41ccbcce20b1ad6c19d684c025bbc9d
allencage <alingabriel.ciuca@gmail.com>
2017-04-25 15:39:46 +0300

    final commit

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var uri = "http://localhost:8080";
+			using (WebApp.Start<Startup>(uri))
+			{
+				Console.WriteLine("Started!");
+				Console.Read();
+				Console.WriteLine("Stopped");
+			}
+		}
+	}
+
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseWelcomePage();
+			//app.Run(ctx =>
+			//{
+			//	return ctx.Response.WriteAsync("Hello World!");
+			//});
+		}
+	}
+}
